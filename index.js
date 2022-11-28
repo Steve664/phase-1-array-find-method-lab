ff00117c17175084b9b02da18f3c3a9f348dbca1
Steve Kariuki <stevekariuki5@gmail.com>
2022-11-28 21:57:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+// code your solution her
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W");
+    return (result != undefined ? result.year : undefined);
+}
